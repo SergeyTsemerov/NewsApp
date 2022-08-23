dd25a3135a8b13490cb6ab2a4ae69575ec467573
SergeyTsemerov <sergeytsemerov@gmail.com>
2022-08-23 18:46:55 +0300

    Done: retrofit implementation

---
+++ app/src/main/java/com/sergeytsemerov/newsapp/ui/Source.kt
@@ -0,0 +1,6 @@
+package com.sergeytsemerov.newsapp.ui
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/sergeytsemerov/newsapp/ui/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.sergeytsemerov.newsapp.ui.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "23f43263b9e5437898d8509022159c57"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
